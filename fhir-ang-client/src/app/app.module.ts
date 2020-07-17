5c8a70178f9d8ae1eae2b88e82701ec7c5c51192
Ibrahim Gokalp <ibrahim.gokalp@turkcell.com.tr>
2020-07-17 17:28:20 +0300

    Added ng-zorro library

--- fhir-ang-client/src/app/app.module.ts
+++ fhir-ang-client/src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { tr_TR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import tr from '@angular/common/locales/tr';
+
+registerLocaleData(tr);
 
 @NgModule({
   declarations: [
@@ -10,9 +22,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: tr_TR }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
