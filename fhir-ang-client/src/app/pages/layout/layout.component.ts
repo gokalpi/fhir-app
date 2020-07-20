58ccb32f61fd68ff1770e908b376e0981d6abb11
Ibrahim Gokalp <ibrahim.gokalp@turkcell.com.tr>
2020-07-20 15:49:56 +0300

    Changed menu and logo

--- fhir-ang-client/src/app/pages/layout/layout.component.ts
+++ fhir-ang-client/src/app/pages/layout/layout.component.ts
@@ -1,14 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent {
   isCollapsed = false;
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  mode = false;
+  dark = false;
+  menus = [
+    {
+      level: 1,
+      title: 'Dashboard',
+      icon: 'dashboard',
+      open: true,
+      selected: false,
+      disabled: false,
+      children: [
+        {
+          level: 2,
+          title: 'My Schedule',
+          url: '/pages/schedule',
+          selected: true,
+          disabled: false,
+        },
+      ],
+    },
+    {
+      level: 1,
+      title: 'Patients',
+      icon: 'team',
+      open: false,
+      selected: false,
+      disabled: false,
+      children: [
+        {
+          level: 2,
+          title: 'List Patients',
+          url: '/pages/patients',
+          selected: false,
+          disabled: false,
+        },
+        {
+          level: 2,
+          title: 'Add Patient',
+          url: '/pages/patients/add',
+          selected: false,
+          disabled: false,
+        },
+      ],
+    },
+    {
+      level: 1,
+      title: 'Examinations',
+      icon: 'read',
+      open: false,
+      selected: false,
+      disabled: false,
+      children: [
+        {
+          level: 2,
+          title: 'List Examinations',
+          url: '/pages/examinations',
+          selected: false,
+          disabled: false,
+        },
+        {
+          level: 2,
+          title: 'Add Examination',
+          url: '/pages/examinations/add',
+          selected: false,
+          disabled: false,
+        },
+      ],
+    },
+  ];
 }
