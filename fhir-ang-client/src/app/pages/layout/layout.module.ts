3fc9d96981ba367e2c0c183895f28fe709a510a9
Ibrahim Gokalp <ibrahim.gokalp@turkcell.com.tr>
2020-07-20 14:14:54 +0300

    Removed static icons

--- fhir-ang-client/src/app/pages/layout/layout.module.ts
+++ fhir-ang-client/src/app/pages/layout/layout.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AllNgZorroAntdModules } from '../../all-ng-zorro-antd.module';
-import { IconsProviderModule } from '../../icons-provider.module';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [
-    CommonModule,
-    LayoutRoutingModule,
-    AllNgZorroAntdModules,
-    IconsProviderModule,
-  ],
+  imports: [CommonModule, LayoutRoutingModule, AllNgZorroAntdModules],
   providers: [],
   exports: [LayoutComponent],
 })
