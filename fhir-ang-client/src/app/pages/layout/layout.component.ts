1df09a80062c6c2f9a932616c0dd60269bcca4a6
gokalpi <gokalpi@gmail.com>
2020-07-28 12:34:50 +0300

    Changed styles for page header and pagination

--- fhir-ang-client/src/app/pages/layout/layout.component.ts
+++ fhir-ang-client/src/app/pages/layout/layout.component.ts
@@ -9,147 +9,46 @@ import { AccountService } from 'src/app/core/services';
 })
 export class LayoutComponent {
   user: User;
-
   isCollapsed = false;
 
-  mode = false;
-  dark = false;
   menus = [
     {
       level: 1,
       title: 'Dashboard',
       icon: 'dashboard',
-      open: true,
-      selected: false,
-      disabled: false,
-      roles: ['Doctor', 'Admin'],
-      children: [
-        {
-          level: 2,
-          title: 'My Schedule',
-          url: '/dashboard',
-          selected: true,
-          disabled: false,
-          roles: ['Doctor'],
-        },
-      ],
+      url: '/dashboard',
     },
     {
       level: 1,
       title: 'Patients',
       icon: 'team',
-      open: false,
-      selected: false,
-      disabled: false,
+      url: '/patients',
       roles: ['Doctor', 'Admin'],
-      children: [
-        {
-          level: 2,
-          title: 'List Patients',
-          url: '/patients',
-          selected: false,
-          disabled: false,
-          roles: ['Doctor', 'Admin'],
-        },
-        {
-          level: 2,
-          title: 'Add Patient',
-          url: '/patients/add',
-          selected: false,
-          disabled: false,
-          roles: ['Doctor', 'Admin'],
-        },
-      ],
     },
     {
       level: 1,
       title: 'Examinations',
       icon: 'read',
-      open: false,
-      selected: false,
-      disabled: false,
+      url: '/examinations',
       roles: ['Doctor', 'Admin'],
-      children: [
-        {
-          level: 2,
-          title: 'List Examinations',
-          url: '/examinations',
-          selected: false,
-          disabled: false,
-          roles: ['Doctor', 'Admin'],
-        },
-        {
-          level: 2,
-          title: 'Add Examination',
-          url: '/examinations/add',
-          selected: false,
-          disabled: false,
-          roles: ['Doctor', 'Admin'],
-        },
-      ],
     },
     {
       level: 1,
       title: 'Settings',
       icon: 'setting',
-      open: false,
-      selected: false,
-      disabled: false,
       roles: ['Admin'],
       children: [
         {
           level: 2,
           title: 'Users',
+          url: '/admin/users',
           icon: 'team',
-          open: false,
-          selected: false,
-          disabled: false,
-          roles: ['Admin'],
-          children: [
-            {
-              level: 3,
-              title: 'List Users',
-              url: '/admin/users',
-              selected: false,
-              disabled: false,
-              roles: ['Admin'],
-            },
-            {
-              level: 3,
-              title: 'Add User',
-              url: '/admin/users/add',
-              selected: false,
-              disabled: false,
-              roles: ['Admin'],
-            },
-          ],
         },
         {
           level: 2,
           title: 'Roles',
           icon: 'safety-certificate',
-          open: false,
-          selected: false,
-          disabled: false,
-          roles: ['Admin'],
-          children: [
-            {
-              level: 3,
-              title: 'List Roles',
-              url: '/admin/roles',
-              selected: false,
-              disabled: false,
-              roles: ['Admin'],
-            },
-            {
-              level: 3,
-              title: 'Add Role',
-              url: '/admin/roles/add',
-              selected: false,
-              disabled: false,
-              roles: ['Admin'],
-            },
-          ],
+          url: '/admin/roles',
         },
       ],
     },
