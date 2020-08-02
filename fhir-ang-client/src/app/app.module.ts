563dc8439ec0415e0fcb767a8e6e0f763fc36bdf
gokalpi <gokalpi@gmail.com>
2020-08-02 13:14:25 +0100

    Set default language to en-us

--- fhir-ang-client/src/app/app.module.ts
+++ fhir-ang-client/src/app/app.module.ts
@@ -7,6 +7,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/** config angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+registerLocaleData(en);
+
+/** config ng-zorro-antd i18n **/
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+
 import { JwtInterceptor, ErrorInterceptor } from './core/interceptors';
 
 @NgModule({
@@ -19,6 +27,7 @@ import { JwtInterceptor, ErrorInterceptor } from './core/interceptors';
     BrowserAnimationsModule,
   ],
   providers: [
+    { provide: NZ_I18N, useValue: en_US },
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
