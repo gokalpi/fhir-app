07db35ee7f4c009bb24562cebc7e8014a2a079d8
Ibrahim Gokalp <ibrahim.gokalp@turkcell.com.tr>
2020-08-04 22:30:05 +0300

    Fixed laboratory observations page

--- fhir-ang-client/src/app/modules/diagnostics/observation/laboratory/laboratory.component.ts
+++ fhir-ang-client/src/app/modules/diagnostics/observation/laboratory/laboratory.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { FhirService } from 'src/app/core/services';
 
 @Component({
-  selector: 'app-laboratory',
+  selector: 'app-observation-laboratory',
   templateUrl: './laboratory.component.html',
-  styleUrls: ['./laboratory.component.css']
+  styleUrls: ['./laboratory.component.css'],
 })
-export class LaboratoryComponent implements OnInit {
+export class LaboratoryComponent implements OnInit, OnDestroy {
+  isLoaded = false;
+  bundleResult: any;
+  observations: any;
+  subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private service: FhirService) {}
 
   ngOnInit(): void {
+    // Get all laboratory observations of the patient
+    this.subscriptions.push(
+      this.service
+        .search({
+          resourceType: 'Observation',
+          params: [
+            'category=laboratory',
+            '_include=*',
+            '_count=1000',
+            '_sort=-date',
+          ],
+        })
+        .subscribe((result: any) => {
+          this.bundleResult = result.entry;
+          this.observations = result.entry.filter(
+            (items) => items.resource.resourceType === 'Observation'
+          );
+          this.isLoaded = true;
+          console.log('bundleResult', this.bundleResult);
+          console.log('Observations', this.observations);
+        })
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+
+  getPatientName(patientId: string): string {
+    if (!patientId || patientId.length === 0) {
+      return '';
+    }
+
+    const patient = this.bundleResult.find(
+      (e) =>
+        e.resource.resourceType === 'Patient' && e.resource.id === patientId
+    );
+
+    if (patient) {
+      const name = patient.resource.name[0];
+      return `${name.prefix ? name.prefix.join(' ') : ''} ${
+        name.given ? name.given.join(' ') : ''
+      } ${name.family}`;
+    }
+
+    return '';
+  }
+
+  getValue(resource: any): string {
+    if (resource.valueQuantity) {
+      return resource.valueQuantity.value + ' ' + resource.valueQuantity.unit; 
+    }
+
+    return '';
+  }
 }
--- fhir-ang-client/src/app/modules/diagnostics/observation/laboratory/laboratory.module.ts
+++ fhir-ang-client/src/app/modules/diagnostics/observation/laboratory/laboratory.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AllNgZorroAntdModules } from 'src/app/all-ng-zorro-antd.module';
 
 import { LaboratoryRoutingModule } from './laboratory-routing.module';
 import { LaboratoryComponent } from './laboratory.component';
-
 
 @NgModule({
   declarations: [LaboratoryComponent],
   imports: [
     CommonModule,
-    LaboratoryRoutingModule
-  ]
+    LaboratoryRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AllNgZorroAntdModules,
+  ],
 })
-export class LaboratoryModule { }
+export class LaboratoryModule {}
