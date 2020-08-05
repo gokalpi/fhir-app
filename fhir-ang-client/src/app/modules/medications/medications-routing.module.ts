662df8a98e8011ec6fc1ba28a977bdbbbe61aaec
Ibrahim Gokalp <ibrahim.gokalp@turkcell.com.tr>
2020-08-05 11:51:42 +0300

    Organized file content

--- fhir-ang-client/src/app/modules/medications/medications-routing.module.ts
+++ fhir-ang-client/src/app/modules/medications/medications-routing.module.ts
@@ -3,10 +3,26 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MedicationsComponent } from './medications.component';
 
-const routes: Routes = [{ path: '', component: MedicationsComponent }, { path: 'medication-requests', loadChildren: () => import('./medication-request/medication-request.module').then(m => m.MedicationRequestModule) }, { path: 'immunizations', loadChildren: () => import('./immunization/immunization.module').then(m => m.ImmunizationModule) }];
+const routes: Routes = [
+  { path: '', component: MedicationsComponent },
+  {
+    path: 'medication-requests',
+    loadChildren: () =>
+      import('./medication-request/medication-request.module').then(
+        (m) => m.MedicationRequestModule
+      ),
+  },
+  {
+    path: 'immunizations',
+    loadChildren: () =>
+      import('./immunization/immunization.module').then(
+        (m) => m.ImmunizationModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MedicationsRoutingModule { }
+export class MedicationsRoutingModule {}
