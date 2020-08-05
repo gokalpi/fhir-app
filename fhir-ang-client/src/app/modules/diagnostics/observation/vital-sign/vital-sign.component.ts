1e91d283e562971e206923094135307fe951bf76
Ibrahim Gokalp <ibrahim.gokalp@turkcell.com.tr>
2020-08-05 11:46:49 +0300

    Added vital sign observations page

--- fhir-ang-client/src/app/modules/diagnostics/observation/vital-sign/vital-sign.component.ts
+++ fhir-ang-client/src/app/modules/diagnostics/observation/vital-sign/vital-sign.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { FhirService } from 'src/app/core/services';
 
 @Component({
   selector: 'app-vital-sign',
   templateUrl: './vital-sign.component.html',
-  styleUrls: ['./vital-sign.component.css']
+  styleUrls: ['./vital-sign.component.css'],
 })
-export class VitalSignComponent implements OnInit {
+export class VitalSignComponent implements OnInit, OnDestroy {
+  isLoading = true;
+  bundleResult: any;
+  observations: any;
+  subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private service: FhirService) {}
 
   ngOnInit(): void {
+    // Get all vital sign observations of the patient
+    this.subscriptions.push(
+      this.service
+        .search({
+          resourceType: 'Observation',
+          params: [
+            'category=vital-signs',
+            '_include=*',
+            '_count=1000',
+            '_sort=-date',
+          ],
+        })
+        .subscribe((result: any) => {
+          this.bundleResult = result.entry;
+          this.observations = result.entry.filter(
+            (items) => items.resource.resourceType === 'Observation'
+          );
+          this.isLoading = false;
+        })
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+
+  getPatientName(patientId: string): string {
+    if (!patientId || patientId.length === 0) {
+      return '';
+    }
+
+    const patient = this.bundleResult.find(
+      (e) =>
+        e.resource.resourceType === 'Patient' && e.resource.id === patientId
+    );
+
+    if (patient) {
+      const name = patient.resource.name[0];
+      return `${name.prefix ? name.prefix.join(' ') : ''} ${
+        name.given ? name.given.join(' ') : ''
+      } ${name.family}`;
+    }
+
+    return '';
+  }
+
+  getVitalSignObservationValue(observation: any): string {
+    if (observation.valueQuantity) {
+      return `${observation.valueQuantity.value} ${observation.valueQuantity.unit}`;
+    }
+
+    const vitalSignValue = [];
+    for (const item of observation.component) {
+      vitalSignValue.push(
+        `${item.code.text}: ${item.valueQuantity.value} ${item.valueQuantity.unit}`
+      );
+    }
+
+    return vitalSignValue.join(', ');
+  }
 }
--- fhir-ang-client/src/app/modules/diagnostics/observation/vital-sign/vital-sign.module.ts
+++ fhir-ang-client/src/app/modules/diagnostics/observation/vital-sign/vital-sign.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AllNgZorroAntdModules } from 'src/app/all-ng-zorro-antd.module';
 
 import { VitalSignRoutingModule } from './vital-sign-routing.module';
 import { VitalSignComponent } from './vital-sign.component';
-
 
 @NgModule({
   declarations: [VitalSignComponent],
   imports: [
     CommonModule,
-    VitalSignRoutingModule
-  ]
+    VitalSignRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AllNgZorroAntdModules,
+  ],
 })
-export class VitalSignModule { }
+export class VitalSignModule {}
