84c347f42beb0b4e82a94d58f5d3884024f51f6d
Ibrahim Gokalp <ibrahim.gokalp@turkcell.com.tr>
2020-08-06 10:52:01 +0300

    Added Encounter details empty module

---
+++ fhir-ang-client/src/app/modules/administration/encounter/encounter-details/encounter-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-encounter-details',
+  templateUrl: './encounter-details.component.html',
+  styleUrls: ['./encounter-details.component.css']
+})
+export class EncounterDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
